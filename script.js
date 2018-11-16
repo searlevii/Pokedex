18ca04b97f949af2f812fc39ad64a2719c494d11
Savon Earle <searle@nyc.yearup.org>
2018-11-16 12:32:15 -0500

    Commit #3

---
+++ script.js
@@ -0,0 +1,14 @@
+let 
+
+
+class Pokemon {
+  constructor(id, name, hp, attack, defense, abilities){
+    this.id = id;
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.abilities = abilities;
+  }
+};
+console.log(Pokemon.name);
