908b2d6db3115427ffabcb7afee1b19284dcc524
Savon Earle <searle@nyc.yearup.org>
2018-11-20 16:27:09 -0500

    Added animated sprites

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 //Axios client provides data for pokemon via the APIs//
-axios.get('http://fizal.me/pokeapi/api/v2/name/greninja.json')
+axios.get('https://pokeapi.co/api/v2/pokemon/greninja-ash/')
  .then(function (response){
    console.log(response.data);
-   let greninjaSprite = document.createElement('img');
-   greninjaSprite.src = response.data.sprites.front_shiny;
-   document.getElementById('greninja').appendChild(greninjaSprite);
+   let greninja = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name, response.data.moves[54].move.name, response.data.moves[54].move.name, response.data.moves[54].move.name, response.data.moves[54].move.name);
+   greninja.sprite()
  });
 axios.get('http://fizal.me/pokeapi/api/v2/name/magearna.json')
   .then(function (response){
     console.log(response.data);
-    let magearnaSprite = document.createElement('img');
-    magearnaSprite.src = response.data.sprites.front_default;
-    document.getElementById('magearna').appendChild(magearnaSprite);
+    let magearna = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name, response.data.moves[10].move.name, response.data.moves[10].move.name, response.data.moves[10].move.name, response.data.moves[10].move.name);
+    magearna.sprite2()
 });
 axios.get('http://fizal.me/pokeapi/api/v2/name/ariados.json')
   .then(function (response){
     console.log(response.data);
-    let ariadosSprite = document.createElement('img');
-    ariadosSprite.src = response.data.sprites.front_default;
-    document.getElementById('ariados').appendChild(ariadosSprite);
+    let ariados = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name, response.data.moves[54].move.name, response.data.moves[54].move.name, response.data.moves[54].move.name, response.data.moves[54].move.name);
+    ariados.sprite3()
 });
-
+class Trainer {
+  
+}
 class Pokemon {
   constructor(id, name, hp, attack, defense, abilities, move1, move2, move3, move4){
     this.id = id;
@@ -33,6 +32,33 @@ class Pokemon {
     this.move2 = move2;
     this.move3 = move3;
     this.move4 = move4;
+  }
+  sprite() {
+    let div = document.getElementById("greninja");
+    let img = document.createElement("img");
+    div.appendChild(img)
+    img.src = "http://play.pokemonshowdown.com/sprites/xyani/"+ this.name +".gif"
+    img.id = "ninjutsu"
+  }
+  sprite2() {
+    let div = document.getElementById("magearna");
+    let img = document.createElement("img");
+    div.appendChild(img)
+    img.src = "http://play.pokemonshowdown.com/sprites/xyani/"+ this.name +".gif"
+    img.id = "gears"
+  }
+  sprite3() {
+    let div = document.getElementById("ariados");
+    let img = document.createElement("img");
+    div.appendChild(img)
+    img.src = "http://play.pokemonshowdown.com/sprites/xyani/"+ this.name +".gif"
+    img.id = "silk"
+  }
+  display(){
+    let hp = document.createElement("h1");
+    hp.innerHTML = this.hp;
+    this.element.appendChild(hp);
+    this.container.appendChild(this.element);
   }
 };
 console.log(Pokemon.name);
