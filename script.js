b76e90b27e3b86ad053bc1e2a2ac91d46b6ddd07
Savon Earle <searle@nyc.yearup.org>
2018-11-26 13:13:57 -0500

    Attempt No. 35 to Fix Pokedex

--- script.js
+++ script.js
@@ -21,21 +21,21 @@ let King = new Trainer();
 
 //Axios client provides data for pokemon via the APIs//
 axios.get('https://fizal.me/pokeapi/api/v2/name/greninja.json')
- .then(function (response){
-   console.log(response.data);
-   let ab = response.data.abilities;
-   let abarray = [];
-   for (i=0; i < ab.length; i++){
-     abarray.push(ab[i].ability.name)
-   }
-   console.log(abarray);
+  .then(function (response){
+    console.log(response.data);
+    let ab = response.data.abilities;
+    let abarray = [];
+    for (i=0; i < ab.length; i++){
+      abarray.push(ab[i].ability.name)
+    }
 
-   let greninja = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat,
-   response.data.abilities[0].ability.name);
-   greninja.sprite()
-   King.add(greninja);
-   greninja.display();
- });
+    console.log(abarray);
+
+    let greninja = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name);
+    greninja.sprite()
+    King.add(greninja);
+    greninja.display1();
+});
 axios.get('https://fizal.me/pokeapi/api/v2/name/magearna.json')
   .then(function (response){
     console.log(response.data);
@@ -44,9 +44,10 @@ axios.get('https://fizal.me/pokeapi/api/v2/name/magearna.json')
     for (i=0; i < ab.length; i++){
       abarray.push(ab[i].ability.name)
     }
+
     console.log(abarray);
 
-    let magearna = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name)
+    let magearna = new Pokemon(response.data.id, response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name);
     magearna.sprite2()
     King.add(magearna);
     magearna.display2();
@@ -76,10 +77,10 @@ class Pokemon {
     this.attack = attack;
     this.defense = defense;
     this.abilities = abilities;
-    this.element = document.getElementById("element");
+    this.element1 = document.getElementById("element1");
     this.element2 = document.getElementById("element2");
     this.element3 = document.getElementById("element3");
-    this.container = document.getElementById("status");
+    this.container = document.getElementById("status1");
     this.container2 = document.getElementById("status2");
     this.container3 = document.getElementById("status3");
   }
@@ -108,31 +109,31 @@ class Pokemon {
     let div = document.getElementById("champion");
 
   }
-  display(){
+  display1(){
     let name = document.querySelector('ol');
     name.innerHTML = "Name: " + this.name;
-    this.element.appendChild(name);
-    this.container.appendChild(this.element);
+    this.element1.appendChild(name);
+    this.container.appendChild(this.element1);
 
     let hp = document.querySelector('ol');
     hp.innerHTML = "HP: " + this.hp;
-    this.element.appendChild(hp);
-    this.container.appendChild(this.element);
+    this.element1.appendChild(hp);
+    this.container.appendChild(this.element1);
 
     let attack = document.querySelector('ol');
     attack.innerHTML = "ATK: " + this.attack;
-    this.element.appendChild(attack);
-    this.container.appendChild(this.element);
+    this.element1.appendChild(attack);
+    this.container.appendChild(this.element1);
 
     let defense = document.querySelector('ol');
     defense.innerHTML = "DEF: " + this.defense;
-    this.element.appendChild(defense);
-    this.container.appendChild(this.element);
+    this.element1.appendChild(defense);
+    this.container.appendChild(this.element1);
 
     let abilities = document.querySelector('ol');
     abilities.innerHTML = "ABILITIES: " + this.abilities;
-    this.element.appendChild(abilities);
-    this.container.appendChild(this.element);
+    this.element1.appendChild(abilities);
+    this.container.appendChild(this.element1);
   }
  display2(){
    let ol = document.querySelector('ol');
